d279c7826d21ba388b37f667fda69124eb271f0e
kimi <sakura_kimi@foxmail.com>
2023-07-08 23:08:51 +0800

    update

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
 - 搜索建议词的数据源使用 JSON 文件： :white_check_mark: 
 - 点击右侧的 「x」按钮，可清除输入框内的文本并隐藏下拉框；
 - 可通过键盘的方向键在下拉框中选择（参考百度），按下回车将建议词回填至输入框； :white_check_mark: 
-- 可使用任意第三方框架和库；(未使用第三方库,采用react原生写法):black_square_button: 
+- 可使用任意第三方框架和库；(采用react原生写法) :white_check_mark: 
 - 除以上提及的交互外，无需实现其它按钮的交互效果； :white_check_mark: 
 
 ### 技术栈:
--- service/app.js
+++ service/app.js
@@ -1,70 +1,29 @@
 const express = require('express');
 const axios = require('axios');
+const data = require('./data.json')
 
 const app = express();
 const port = 3001;
-
-const data = [
-    {
-        "text": "中国银行",
-        "id": 1
-    },
-    {
-        "text": "中国邮政",
-        "id": 2
-    },
-    {
-        "text": "中国移动",
-        "id": 3
-    },
-    {
-        "text": "上海天气",
-        "id": 4
-    },
-    {
-        "text": "中国电信",
-        "id": 5
-    },
-    {
-        "text": "上海市政府",
-        "id": 6
-    },
-    {
-        "text": "上海人口",
-        "id": 7
-    },
-    {
-        "text": "北京天气",
-        "id": 8
-    },
-    {
-        "text": "北京时间",
-        "id": 9
-    },
-    {
-        "text": "北京旅游攻略",
-        "id": 10
-    }
-]
+const fileId = '1iT2750MGszpzi5mrCHCFmF8HYsCiHA0I';
 
 app.get('/api/get-google-files', async (req, res) => {
-    setTimeout(() => {
-        console.info(data)
-        res.send({
-            success: true,
-            resources: data,
-        });
-        return
-    }, 200)
+    try {
+        setTimeout(() => {
+            console.info(data)
+            res.send({
+                success: true,
+                resources: data,
+            });
+            return
+        }, 200)
 
-    // const url = 'https://drive.google.com/file/d/1iT2750MGszpzi5mrCHCFmF8HYsCiHA0I/view';
-    // try {
-    //     const response = await axios.get(url);
-    //     res.send(response.data);
-    // } catch (error) {
-    //     console.error(error);
-    //     res.status(500).send('Error occurred while fetching the data.');
-    // }
+        const response = await axios.get(`https://drive.google.com/file/d/files/${fileId}?alt=media`, {
+        });
+        res.send(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error occurred while fetching the data.');
+    }
 });
 
 app.listen(port, () => {
